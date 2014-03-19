9a9d401a58ec20abc8fcf03f7b13f4b64cfbdae5
Iván López <lopez.ivan@gmail.com>
2014-03-19 23:45:27 +0100

    Local AST Transformation example

---
+++ localAST/src/main/groovy/greach2014/Version.groovy
@@ -0,0 +1,15 @@
+package greach2014
+
+import org.codehaus.groovy.transform.GroovyASTTransformationClass
+
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+
+@Retention(RetentionPolicy.SOURCE)
+@Target([ElementType.TYPE])
+@GroovyASTTransformationClass("greach2014.VersionASTTransformation")
+@interface Version {
+    String value()
+}
